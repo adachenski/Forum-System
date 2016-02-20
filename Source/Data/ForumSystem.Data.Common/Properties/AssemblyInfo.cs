24232cb72f77bd500c55a7c7a7dd8d763805e665
adachenski <adachenski@aol.com>
2016-02-20 02:39:26 +0200

    FeedbakList View, Controller Addred

--- Source/Data/ForumSystem.Data.Common/Properties/AssemblyInfo.cs
+++ Source/Data/ForumSystem.Data.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ForumSystem.Common")]
+[assembly: AssemblyTitle("ForumSystem.Data.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ForumSystem.Common")]
+[assembly: AssemblyProduct("ForumSystem.Data.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3c8c28d6-4949-42a6-9543-83024d546533")]
+[assembly: Guid("318822b6-1850-48df-a517-60ddaf356bb2")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Source/Data/ForumSystem.Data.Models/ApplicationUser.cs
+++ Source/Data/ForumSystem.Data.Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using ForumSystem.Data.Common.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -9,8 +10,18 @@ using System.Threading.Tasks;
 
 namespace ForumSystem.Data.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IAuditInfo, IDeletableEntity
     {
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
